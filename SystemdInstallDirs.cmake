fd998919c0e51cd98272c5bd8f2601f15fcfb15c
Damien Flament <damien.flament@gmx.com>
2019-03-04 08:46:56 +0100

    Add SystemdInstallDirs module

---
+++ SystemdInstallDirs.cmake
@@ -0,0 +1,57 @@
+# Copyright © 2019 Damien Flament
+#
+# This file is part of CMake Modules.
+#
+# CMake Modules is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# CMake Modules is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CMake Modules.  If not, see <https://www.gnu.org/licenses/>.
+
+#[=======================================================================[.rst:
+SystemdInstallDirs
+------------------
+
+Define Systemd standard installation directories.
+
+Result Variables
+^^^^^^^^^^^^^^^^
+
+Inclusion of this module defines the following variables:
+
+``SYSTEMD_INSTALL_<dir>``
+
+  Destination for files of a given type.  This value may be passed to
+  the ``DESTINATION`` options of :command:`install` commands for the
+  corresponding file type.
+
+where ``<dir>`` is one of:
+
+``SYSTEM_UNITS_DIR``
+  system units (``LIBDIR/systemd/system``)
+``USER_UNITS_DIR``
+  user units (``LIBDIR/systemd/user``)
+
+#]=======================================================================]
+
+include_guard(GLOBAL)
+
+include(GNUInstallDirs)
+
+set(SYSTEMD_INSTALL_SYSTEM_UNITS_DIR "${CMAKE_INSTALL_LIBDIR}/systemd/system"
+  CACHE PATH "Systemd system units directory (LIBDIR/systemd/system)")
+
+set(SYSTEMD_INSTALL_USER_UNITS_DIR "${CMAKE_INSTALL_LIBDIR}/systemd/user"
+  CACHE PATH "Systemd user units directory (LIBDIR/systemd/user)")
+
+mark_as_advanced(
+  SYSTEMD_INSTALL_SYSTEM_UNITS_DIR
+  SYSTEMD_INSTALL_USER_UNITS_DIR
+)
