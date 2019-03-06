ab6c56226cba59d4ef87b8773785ba672789e878
Damien Flament <damien.flament@gmx.com>
2019-03-06 18:46:59 +0100

    Use SPDX license identfier for license notice

--- FindGDBusCodegen.cmake
+++ FindGDBusCodegen.cmake
@@ -1,19 +1,6 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
 # Copyright © 2019 Damien Flament
-#
 # This file is part of CMake Modules.
-#
-# CMake Modules is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# CMake Modules is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with CMake Modules.  If not, see <https://www.gnu.org/licenses/>.
 
 #[=======================================================================[.rst:
 FindGDBusCodegen
